63011288153246c1ccb2777123551bf1043e928e
Hoon Hyun <hoon.hyun@farragut.com>
2017-04-03 09:10:33 -0400

    initial commit

---
+++ SeleniumParallelTest/SeleniumParallelTest/Hooks.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
+using System;
+
+namespace SeleniumParallelTest
+{
+    public enum BrowserType
+    {
+        Chrome,
+        Firefox,
+        IE
+    }
+    [TestFixture]
+    public class Hooks : Base
+    {
+        private BrowserType _browserType;
+        public Hooks(BrowserType browser)
+        {
+            _browserType = browser; 
+
+        }
+
+
+        [SetUp]
+        public void InitializeTest()
+        {
+            // Local
+            ChooseDriverInstance(_browserType);
+
+            // Seleniumgrid in Docker container
+
+            //ChooseWebDriver(_browserType); 
+        }
+
+        public void ChooseWebDriver(BrowserType browserType)
+        {
+            if (browserType == BrowserType.Chrome)
+            {
+                DesiredCapabilities cap = DesiredCapabilities.Chrome();
+                cap.SetCapability("version", "");
+                cap.SetCapability("platform", "LINUX");
+                Driver = new RemoteWebDriver(new Uri("http://localhost:4446/wd/hub"), cap); 
+            }
+            if (browserType == BrowserType.Firefox)
+            {
+                DesiredCapabilities cap = DesiredCapabilities.Firefox();
+                cap.SetCapability("version", "");
+                cap.SetCapability("platform", "LINUX");
+                Driver = new RemoteWebDriver(new Uri("http://localhost:4446/wd/hub"), cap);
+            }
+
+        }
+        private void ChooseDriverInstance(BrowserType browserType)
+        {
+            if(browserType == BrowserType.Chrome)
+               Driver = new ChromeDriver();
+            if (browserType == BrowserType.Firefox)
+                Driver = new FirefoxDriver();
+        }
+    }
+}
---
+++ SeleniumParallelTest/SeleniumParallelTest/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace SeleniumParallelTest
+{
+    [TestFixture]
+    [Parallelizable]
+    public class FirefoxTesting : Hooks
+    {
+        public FirefoxTesting() : base(BrowserType.Firefox)
+        {
+            // browser test
+        }
+
+        [Test]
+        public void FirefoxGoogleTest()
+        {
+            Driver.Navigate().GoToUrl("http://www.google.com");
+
+            Driver.FindElement(By.Name("q")).SendKeys("Selenium");
+
+            //System.Threading.Thread.Sleep(20000); 
+
+            Driver.FindElement(By.Name("btnG")).Click();
+
+            Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true), 
+                                              "The text Selenium doesn't exist"); 
+        }
+    }
+
+    [TestFixture]
+    [Parallelizable]
+    public class ChromeTesting : Hooks
+    {
+        public ChromeTesting() : base(BrowserType.Chrome)
+        {
+        }
+
+        [Test]
+        public void ChromeGoogleTest()
+        {
+            Driver.Navigate().GoToUrl("http://www.google.com");
+
+            Driver.FindElement(By.Name("q")).SendKeys("ExecuteAutomation");
+
+            Driver.FindElement(By.Name("btnG")).Click();
+
+            Assert.That(Driver.PageSource.Contains("ExecuteAutomation"), Is.EqualTo(true),
+                                              "The text ExecuteAutomation doesn't exist");
+        }
+    }
+}
