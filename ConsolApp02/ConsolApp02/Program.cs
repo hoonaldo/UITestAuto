332a15b332a4dbca11a5dfadc4cd2c05ba960e5c
Hoon Hyun <hoon.hyun@gmail.com>
2017-04-04 09:31:22 -0400

    ConsoleApp for Practice C#

---
+++ ConsolApp02/ConsolApp02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolApp02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] customers = new string[] { "smith", "hill", "fox", "cox" };
+
+            using ( StreamWriter sw = new StreamWriter("customers.txt"))
+            {
+                foreach (string cust in customers)
+                {
+                    sw.WriteLine(cust);
+                }
+            }
+
+            string custName = "";
+            List<string> clients = new List<string> { "" }; 
+
+            using (StreamReader sr = new StreamReader("customers.txt"))
+            {
+                while((custName = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(custName);
+                    clients.Add(custName); 
+                }
+            }
+
+            foreach (string name in clients)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
